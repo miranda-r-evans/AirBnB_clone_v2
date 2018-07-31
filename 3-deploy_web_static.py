5932ba9263cb0737cbc58a06080a8c6766574fa3
mirandarevans <miranda.r.evans@gmail.com>
2018-07-31 23:08:52 +0000

    fixing 3 syntax

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -35,25 +35,25 @@ def do_deploy(archive_path):
         if put(archive_path, '/tmp/{}'.format(filename)).failed is True:
             return False
 
-            if run('mkdir -p {}'.format(data_path)).failed is True:
-                return False
-            my_command = 'tar -xzf /tmp/{} -C {}'.format(filename, data_path)
-            if run(my_command).failed is True:
-                return False
-            if run('rm /tmp/{}'.format(filename)).failed is True:
-                return False
-            my_command = 'mv {}/web_static/* {}'.format(data_path, data_path)
-            if run(my_command).failed is True:
-                return False
-            if run('rm -rf {}/web_static'.format(data_path)).failed is True:
-                return False
-            if run('rm -rf /data/web_static/current').failed is True:
-                return False
-            my_command = 'ln -s {} /data/web_static/current'.format(data_path)
-            if run(my_command).failed is True:
-                return False
-        except:
+        if run('mkdir -p {}'.format(data_path)).failed is True:
             return False
+        my_command = 'tar -xzf /tmp/{} -C {}'.format(filename, data_path)
+        if run(my_command).failed is True:
+            return False
+        if run('rm /tmp/{}'.format(filename)).failed is True:
+            return False
+        my_command = 'mv {}/web_static/* {}'.format(data_path, data_path)
+        if run(my_command).failed is True:
+            return False
+        if run('rm -rf {}/web_static'.format(data_path)).failed is True:
+            return False
+        if run('rm -rf /data/web_static/current').failed is True:
+            return False
+        my_command = 'ln -s {} /data/web_static/current'.format(data_path)
+        if run(my_command).failed is True:
+            return False
+    except:
+        return False
 
     return True
 
