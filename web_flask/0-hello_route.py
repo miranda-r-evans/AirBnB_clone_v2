670751ce62dfc347c37944f9edc1d3ce525920dd
mirandarevans <miranda.r.evans@gmail.com>
2018-08-14 22:13:03 +0000

    dummy readme and doc for 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    '''
+        testing basic flask app
+    '''
     return "Hello HBNB!"
 
 app.run(host='0.0.0.0', port=5000, debug=True)
